dbda28889a0c3f4d5c5721d5941ce2e5aa3a86e5
Adam Bonsu <adam@bonsu.io>
2024-11-05 13:28:08 +0000

    add functional test

---
+++ src/test/java/com/example/functional/HelloWorldFunctionalTest.java
@@ -0,0 +1,28 @@
+package com.example.functional;
+
+import org.testng.annotations.Test;
+
+import com.example.core.GreetingService;
+import com.example.core.domain.Greeting;
+import com.example.core.ports.output.GreetingRepository;
+
+import static org.testng.Assert.*;
+public class HelloWorldFunctionalTest {
+
+    @Test
+    public void testHelloWorldOutput() {
+        String expectedOutput = "Hello World";
+        String actualOutput = getHelloWorldOutput();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    private String getHelloWorldOutput() {
+        GreetingRepository greetingRepository = () -> "Hello %s";
+        GreetingService service = new GreetingService(greetingRepository);
+
+        Greeting greeting = service.sayHello("World");
+        return greeting.getMessage();
+    }
+    
+}
